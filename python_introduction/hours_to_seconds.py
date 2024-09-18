5c9de35c4ca74c0be0b05a9513e41eac094e8330
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-09-18 09:39:10 -0700

    edited hours to second file

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,3 @@
 hours = 2
-one_hour_to_minutes = 60 
-one_hour_to_seconds = one_hour_to_minutes * 60
-one_hour_to_seconds = hours * one_hour_to_seconds
+one_hour_to_seconds = hours * 3600
 print(hours, "hour(s) is", one_hour_to_seconds, "seconds")
