54bf9430a972e9da53b90ad1347ddc67424ca8d8
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-09-19 23:49:36 -0700

    added control flow directory

--- future_age_calculator.py
+++
@@ -1,6 +0,0 @@
-num_of_years = int(input("How old are you?  "))
-current_year = 2023
-future_year = 2050
-years_left = future_year - current_year
-future_age = years_left + 27
-print("In", future_year, "you will be", future_age, "years old")
--- hours_to_seconds.py
+++
@@ -1,5 +0,0 @@
-hours = 2
-one_hour_to_minutes = 60 
-one_hour_to_seconds = one_hour_to_minutes * 60
-one_hour_to_seconds = hours * one_hour_to_seconds
-print(hours, "hour(s) is", one_hour_to_seconds, "seconds")
