afa84856412ceea653c1a6ce87fa12da303b194c
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-09-22 10:10:35 -0700

    added a correction to a file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,14 +1,9 @@
-priority = input("Enter its priority (high, medium, low): ")
-time_bound = input("Is it time-bound (yes or no): ")
-match priority:
-    case "high":
-        if time_bound == "yes" or "no":
-            print(f"Reminder: {task} is a high priority that requires immediate attetion today!")
-    case "medium":
-        if time_bound == "yes" or "no":
-            print(f"Okey, {task} is a medium priority that must be planned accordingly!")
-    case "low":
-        if time_bound == "no":
-            print(f"Note: {task} is a low priority task. Consider completing it when you have free time.")
-        else:
-            print(f"If {task} is time-bound, it is okey for you!")
+weather = input("what is the weather like today? (sunny/rainy/cold) ").lower()
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
