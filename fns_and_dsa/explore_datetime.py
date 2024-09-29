a78d917d8fa3d142ee2ad44d4372cd99076e0a38
Shambo-arch <waltzbellmusic2014@gmail.com>
2024-09-29 14:10:58 -0700

    added another file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -13,6 +13,6 @@ def calculate_future_date(days_to_add):
 
 display_current_datetime()
 
-days = 10
+days = int(input("Enter the number of days to add to the current date: "))
 calculate_future_date(days)
 
